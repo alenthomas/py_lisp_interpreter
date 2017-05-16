3b746632a05c2124610290a866683439124f41c0
Alen Thomas <alenthomas9@gmail.com>
2017-05-16 16:53:33 +0530

    adds a hard-coded dummy eval; evaluating (+ num num)

--- py_lisp.py
+++ py_lisp.py
@@ -1,3 +1,5 @@
+from py_env import global_env
+
 def tokenize(string):
     '''
     program = "(begin (define r 10))"
@@ -8,6 +10,10 @@ def tokenize(string):
     return tokens
 
 def atom(token):
+    '''
+    return the value of token
+    precedence: int, float, str
+    '''
     try:
         return int(token)
     except ValueError:
@@ -17,6 +23,10 @@ def atom(token):
             return str(token)
 
 def read_from_tokens(tokens):
+    '''
+    recursivley reads through tokens
+    returns an AST in the form of a list
+    '''
     if not tokens:
         raise SyntaxError("unexpected EOF while reading")
     token = tokens.pop(0)
@@ -39,19 +49,19 @@ def parse():
     '''
     pass
 
-def eval():
-    '''
-    eval(parse(program))
-    314.1592653589793
-    '''
-    pass
-
+def eval(x, env):
+    if x[0] == 'def':
+        pass
+    else:
+        return global_env[x[0]](x[1], x[2]) # test for add
 def interface():
     string = input()
     tokens=tokenize(string)
     val = read_from_tokens(tokens)
     print("tokens", tokens)
     print("AST", val)
+    result = eval(val, global_env)
+    print("Result", result)
 
 
 
