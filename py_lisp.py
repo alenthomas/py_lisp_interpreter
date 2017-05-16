dc3fd42f860c9c7f5ef2dc738f900603cf78426f
Alen Thomas <alenthomas9@gmail.com>
2017-05-16 12:47:26 +0530

    defines what each function is supposed to do

--- py_lisp.py
+++ py_lisp.py
@@ -1,7 +1,24 @@
+def tokenize():
+    '''
+    program = "(begin (define r 10))"
+    tokenize(program)
+    ['(', 'begin', '(', 'define', 'r', '10', ')', ')']
+    '''
+    pass
+
 def parse():
+    '''
+    program = "(begin (define r 10) (* pi (* r r)))"
+    parse(program)
+    ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
+    '''
     pass
 
 def eval():
+    '''
+    eval(parse(program))
+    314.1592653589793
+    '''
     pass
 
 def input():
