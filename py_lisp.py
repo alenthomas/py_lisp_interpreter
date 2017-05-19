ec23e8df63b4f36ed145dd75240665bc021762ff
Alen Thomas <alenthomas9@gmail.com>
2017-05-19 11:09:42 +0530

    removes print statements used for dev

--- py_lisp.py
+++ py_lisp.py
@@ -1,19 +1,10 @@
 from py_env import global_env
 
 def tokenize(string):
-    '''
-    program = "(begin (define r 10))"
-    tokenize(program)
-    ['(', 'begin', '(', 'define', 'r', '10', ')', ')']
-    '''
     tokens = string.replace("(", " ( ").replace(")", " ) ").split()
     return tokens
 
 def atom(token):
-    '''
-    return the value of token
-    precedence: int, float, str
-    '''
     try:
         return int(token)
     except ValueError:
@@ -23,58 +14,29 @@ def atom(token):
             return str(token)
 
 def lisp_ast(tokens):
-    '''
-    recursivley reads through tokens
-    returns an AST in the form of a list
-    '''
     if not tokens:
         raise SyntaxError("unexpected EOF while reading")
     token = tokens.pop(0)
     if token is "(":
-        new_list = list()
+        _list = list()
         while tokens[0] is not ")":
-            new_list.append(lisp_ast(tokens))
+            _list.append(lisp_ast(tokens))
         tokens.pop(0)
-        return new_list
+        return _list
     elif token is ")":
         raise SyntaxError("unexpected ')'")
     else:
         return atom(token)
 
 def parse(lisp_string):
-    '''
-    program = "(begin (define r 10) (* pi (* r r)))"
-    parse(program)
-    ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
-    '''
     return lisp_ast(tokenize(lisp_string))
 
 def lisp_eval(x, env):
-    '''
-    Evaluate an expression in an environment
-    '''
-    print("first x:", x)
     if isinstance(x, str):
-        '''
-        checks for string;
-        specifically our keywords/operators
-        if found returns the corresponding python function
-        '''
-        print("isinstance str:", x)
-        print("env[x]:", env[x])
         return env[x]
     elif not isinstance(x, list):
-        '''
-        checks if not a list
-        else it should be a literal specifically atom
-        '''
-        print("isinstance list:", x)
         return x
     elif x[0] == "if":
-        '''
-        if the condition is 'if' evaluate it in python
-        call the eval with the output of if expression
-        '''
         (_, condition, true_exp, false_exp) = x
         exp = (true_exp if lisp_eval(condition, env) else false_exp)
         return lisp_eval(exp, env)
@@ -82,26 +44,14 @@ def lisp_eval(x, env):
         (_, var, exp) = x
         env[var] = lisp_eval(exp, env)
     else:
-        '''
-        if not (list, keyword, number)
-        probably expression
-        '''
-        print("in else, calls eval with first arg:", x[0])
-        proc = lisp_eval(x[0], env)
-        print("proc :", proc)
-        print("x[1:] :", x[1:])
+        function = lisp_eval(x[0], env)
         args = [lisp_eval(arg, env) for arg in x[1:]]
-        print("args :", args)
-        print("proc(*args) :", proc(*args))
-        return proc(*args)
+        return function(*args)
 
 def interface():
     string = input()
     parsed = parse(string)
-    print("AST", parsed)
     result = lisp_eval(parsed, global_env)
-    print("Result", result)
-
 
 if __name__ == "__main__":
     interface()
