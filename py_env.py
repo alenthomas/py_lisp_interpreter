fe2aa6da73d561238a7c60cc38c1aae6dbfa51d0
Alen Thomas <alenthomas9@gmail.com>
2017-05-23 12:04:57 +0530

    maps lisp operators to python functions

--- py_env.py
+++ py_env.py
@@ -17,15 +17,18 @@ class Env(dict):
 def standard_env():
     env = Env()
     env.update({
-        '+': op.add,
-        '-': op.sub,
-        '*': op.mul,
-        '/': op.truediv,
-        '>': op.gt,
-        '<': op.lt,
-        '=': op.eq,
-        'cons': lambda x: x[0],
-        'cdr': lambda x: x[1:],
+        "+"   : op.add,
+        "-"   : op.sub,
+        "*"   : op.mul,
+        "/"   : op.truediv,
+        ">"   : op.gt,
+        "<"   : op.lt,
+        "="   : op.eq,
+        ">="  : op.ge,
+        "<="  : op.le,
+        "car" : lambda x: x[0],
+        "cdr" : lambda x: x[1:],
+        "cons": lambda x,y: [x] + y,
     })
     return env
 
