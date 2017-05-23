afcb8109947bddd31578c4c0390fb1af549ecd9a
Alen Thomas <alenthomas9@gmail.com>
2017-05-23 11:27:39 +0530

    removes the global_env as parameter; since it is specified in the func_def

--- lisp_repl.py
+++ lisp_repl.py
@@ -1,8 +1,10 @@
 from py_lisp import lisp_eval, parse
-from py_env import global_env
 
 def repl(prompt="=>"):
     while True:
-        val = lisp_eval(parse(input(prompt)), global_env)
+        val = lisp_eval(parse(input(prompt)))
         if val is not None:
             print(val)
+
+if __name__ == "__main__":
+    repl()
